08ce53fbb47cb253682d58c610a0d712e50bdd90
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-01-29 14:57:59 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonComponent from "./ButtonComponent";
+import ImageComponent from "./ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <ButtonComponent text="Salva" />
+
+      <ImageComponent
+        url="https://media.istockphoto.com/id/517188688/it/foto/paesaggio-di-montagna.jpg?s=612x612&w=0&k=20&c=3-yra8c7GyAenb5DTfF1x5XKFqGB1Gp6vXAtTf5fDNM="
+        textAlt="Foto Paesaggio"
+      />
+      <ButtonComponent text="Ciao" />
+      <ImageComponent
+        url="https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg"
+        textAlt="Foto Paesaggio 2"
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +29,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
